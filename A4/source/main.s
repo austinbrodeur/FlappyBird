c5b6cb2bc2adb2a5e3675a6eb8d10116dc19df08
austinbrodeur <austinbrodeur@Austins-MacBook-Pro.local>
2017-06-15 12:56:21 -0600

    Initial Files

---
+++ A4/source/main.s
@@ -0,0 +1,30 @@
+.section    .init
+.globl     _start
+
+_start:
+    b       main
+    
+.section .text
+
+main:
+    	mov    	sp, #0x8000 // Initializing the stack pointer
+	bl	EnableJTAG // Enable JTAG
+
+
+	bl	InitFrameBuffer
+	bl	clearScreen
+	bl	drawBG
+
+
+haltLoop$:
+	b	haltLoop$
+
+end:
+
+	pop		{r4, r5, pc}
+
+
+
+
+.section .data  
+
